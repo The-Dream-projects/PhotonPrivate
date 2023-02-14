f09009507adc1cad3a75a222d90cc5cbfa27311a
EatingSouls <timmy120403@gmail.com>
2023-02-14 13:01:43 +0100

    Initial Command

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).send('Not found');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+module.exports = app;
