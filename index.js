c3d764e5d1aaf37c2599762a2f881fe48a8cddb0
EatingSouls <timmy120403@gmail.com>
2023-02-22 18:10:53 +0100

    Mobile update - to fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-
+const useragent = require('express-useragent');
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(useragent.express());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  if(req.useragent.isMobile) {
+    return res.render('mobile/index');
+  }
   res.render('desktop/index', { title: 'Express' });
 });
 
